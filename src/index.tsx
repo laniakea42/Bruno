d745adfd4c54cf2295c70c85d11322e1b4af7368
alissa_y <alissa.yaitskaya@equilibrium.io>
2023-01-15 23:06:50 +0300

    fix global

--- src/index.tsx
+++ src/index.tsx
@@ -6,13 +6,13 @@ import { ThemeProvider } from "styled-components";
 import { baseTheme } from "./styles/theme";
 
 const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
+  document.getElementById("root") as HTMLElement,
 );
 root.render(
   <React.StrictMode>
-    {/* <ThemeProvider theme={{ baseTheme }}> */}
+    <ThemeProvider theme={{ baseTheme }}>
       <GlobalStyle />
       <Index />
-    {/* </ThemeProvider> */}
-  </React.StrictMode>
+    </ThemeProvider>
+  </React.StrictMode>,
 );
